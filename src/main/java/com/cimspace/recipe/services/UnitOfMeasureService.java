515f7f2d6c8f50b15049210f397268bf0b408cb7
Ike Chukwu <imchukwu@gmail.com>
2020-05-08 14:31:50 +0100

    config

---
+++ src/main/java/com/cimspace/recipe/services/UnitOfMeasureService.java
@@ -0,0 +1,13 @@
+package com.cimspace.recipe.services;
+
+import com.cimspace.recipe.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/28/17.
+ */
+public interface UnitOfMeasureService {
+
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
---
+++ src/main/java/com/cimspace/recipe/services/UnitOfMeasureServiceImpl.java
@@ -0,0 +1,34 @@
+package com.cimspace.recipe.services;
+
+import com.cimspace.recipe.commands.UnitOfMeasureCommand;
+import com.cimspace.recipe.converters.UnitOfMeasureToUnitOfMeasureCommand;
+import com.cimspace.recipe.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+/**
+ * Created by jt on 6/28/17.
+ */
+@Service
+public class UnitOfMeasureServiceImpl implements UnitOfMeasureService {
+
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+    private final UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand;
+
+    public UnitOfMeasureServiceImpl(UnitOfMeasureRepository unitOfMeasureRepository, UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand) {
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.unitOfMeasureToUnitOfMeasureCommand = unitOfMeasureToUnitOfMeasureCommand;
+    }
+
+    @Override
+    public Set<UnitOfMeasureCommand> listAllUoms() {
+
+        return StreamSupport.stream(unitOfMeasureRepository.findAll()
+                .spliterator(), false)
+                .map(unitOfMeasureToUnitOfMeasureCommand::convert)
+                .collect(Collectors.toSet());
+    }
+}
